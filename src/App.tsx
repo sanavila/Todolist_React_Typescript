1b017d43c3ea54cb0a8d31e02ebde96a41a380f0
sanavila <rute.leone@hotmail.com>
2022-05-23 16:20:04 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { Item } from './types/Item';
+import * as C from './App.styles';
+import { ListItem } from './components/ListItem';
 
-function App() {
+const App = () => {
+  
+  const [list, setList] = useState<Item[]>([
+      {id: 1, name: 'Comprar bolo na padaria', done: false},
+      {id: 2, name: 'Comprar pão na padaria', done: true}
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <C.Container>
+     <C.Area>
+        <C.Header>Lista de Tarefas</C.Header> 
+         
+        {/*Área para adesão de itens na lista*/}
+
+        {list.map((item, index)=>(
+          <ListItem key={index} item={item} />
+        ))}
+
+     </C.Area>    
+   </C.Container>
   );
 }
 
